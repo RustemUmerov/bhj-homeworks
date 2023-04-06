72863db9b2d3a6b50865228157b6f7a6b4c44499
Rustem Umerov <rustem.amdy.umerov@gmail.com>
2023-04-07 01:53:07 +0300

    Загрузка больших файлов готово

--- async-requests/progressbar/task.js
+++ async-requests/progressbar/task.js
@@ -1,27 +1,16 @@
-const fileInput = document.querySelector('input[type=file]');
-const progressBar = document.getElementById('progress');
-
-fileInput.addEventListener('change', function () {
-  const xhr = new XMLHttpRequest();
-  const formData = new FormData();
-  formData.append('file', fileInput.files[0]);
-
-  xhr.open('POST', 'https://students.netoservices.ru/nestjs-backend/upload');
-
-  xhr.upload.addEventListener('progress', function (event) {
-    if (event.lengthComputable) {
-      const percentComplete = event.loaded / event.total;
-      progressBar.value = percentComplete;
-    }
-  }, false);
-
-  xhr.addEventListener('load', function (event) {
-    // Обработка завершения загрузки
-  }, false);
-
-  xhr.addEventListener('error', function (event) {
-    // Обработка ошибок
-  }, false);
-
-  xhr.send(formData);
-});
+ const fileInput = document.getElementById('file');
+  const progress = document.getElementById('progress');
+  
+  fileInput.addEventListener('change', () => {
+    const file = fileInput.files[0];
+    const formData = new FormData();
+    formData.append('file', file);
+    
+    const xhr = new XMLHttpRequest();
+    xhr.open('POST', 'https://example.com/upload');
+    xhr.upload.addEventListener('progress', (event) => {
+      const percent = (event.loaded / event.total) * 100;
+      progress.value = percent;
+    });
+    xhr.send(formData);
+  });
